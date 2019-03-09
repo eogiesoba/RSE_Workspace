6b8cd5d6cb3b8171086882df65fa9cfede5a85dc
eogiesoba <eogiesoba@gmail.com>
2019-03-08 20:55:08 -0600

    Update 1D Gaussian Code to include proper variance symbol

--- practice/1D_Guassian_Filter.cpp
+++ practice/1D_Guassian_Filter.cpp
@@ -24,21 +24,21 @@ int main()
 {
     //Measurements and measurement variance
     double measurements[5] = { 5, 6, 7, 9, 10 };
-    double measurement_sig = 4;
+    double measurement_sig2 = 4;
     
     //Motions and motion variance
     double motion[5] = { 1, 1, 2, 1, 1 };
-    double motion_sig = 2;
+    double motion_sig2 = 2;
     
     //Initial state
     double mu = 0;
-    double sig = 1000;
+    double sig2 = 1000;
 
     for (int i = 0; i < sizeof(measurements) / sizeof(measurements[0]); i++) {
-        tie(mu, sig) = measurement_update(mu, sig, measurements[i], measurement_sig);
-        printf("state_update:  [%f, %f]\n", mu, sig);
-        tie(mu, sig) = state_prediction(mu, sig, motion[i], motion_sig);
-        printf("state_predict: [%f, %f]\n", mu, sig);
+        tie(mu, sig2) = measurement_update(mu, sig2, measurements[i], measurement_sig2);
+        printf("update:  [%f, %f]\n", mu, sig2);
+        tie(mu, sig2) = state_prediction(mu, sig2, motion[i], motion_sig2);
+        printf("predict: [%f, %f]\n", mu, sig2);
     }
 
     return 0;
