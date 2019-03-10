18b7262f2f19fc636d6089010d3506c7c09475e2
eogiesoba <eogiesoba@gmail.com>
2019-03-10 14:14:21 -0500

    Add comments to matrices and formulas

--- practice/Multidimensional_KF.cpp
+++ practice/Multidimensional_KF.cpp
@@ -14,28 +14,28 @@ tuple<MatrixXf, MatrixXf> kalman_filter(MatrixXf x, MatrixXf P, MatrixXf u, Matr
     for (int n = 0; n < sizeof(measurements) / sizeof(measurements[0]); n++) {
 
         // Measurement Update
-        MatrixXf Z(1, 1);
+        MatrixXf Z(1, 1);  // Define a 1 row by 1 col matrix Z
         Z << measurements[n];
 
         MatrixXf y(1, 1);
         y << Z - (H * x);
 
-        MatrixXf S(1, 1);
-        S << H * P * H.transpose() + R;
+        MatrixXf S(1, 1); 
+        S << H * P * H.transpose() + R; // Calculate S for measurement update
 
-        MatrixXf K(2, 1);
-        K << P * H.transpose() * S.inverse();
+        MatrixXf K(2, 1); // Define a 2 row by 1 col matrix K
+        K << P * H.transpose() * S.inverse(); // Calculate of K --> Kalman Gain for measurement update
 
-        x << x + (K * y);
+        // Measurement Update based of robot sensor
+        x << x + (K * y); // x = x' + Ky; where x is the posterior state
+        P << (I - (K * H)) * P; // P = (I - KH)P'; where P is the posterior covariance
 
-        P << (I - (K * H)) * P;
-
-        // Prediction
-        x << (F * x) + u;
-        P << F * P * F.transpose();
+        // State Prediction, based of motion of robots
+        x << (F * x) + u; // x' = (F * x) + u; u is the noise with assumption of zero noise
+        P << F * P * F.transpose(); // P' = (F * P * Ft) + Q ; Q is the noise with assumption of zero noise
     }
 
-    return make_tuple(x, P);
+    return make_tuple(x, P); //This will return the last prediction values for state and covariance.
 }
 
 int main()
@@ -63,8 +63,8 @@ int main()
     	 0, 1; 
 
     tie(x, P) = kalman_filter(x, P, u, F, H, R, I);
-    cout << "x= " << x << endl; //x value after 3 iterations of the multidimensional KF
-    cout << "P= " << P << endl; //P value after 3 iterations of the multidimensional KF
+    cout << "x= " << x << endl; //last prediction of x value after 3 iterations of the multidimensional KF
+    cout << "P= " << P << endl; //last prediction of P value after 3 iterations of the multidimensional KF
 
     return 0;
 }
